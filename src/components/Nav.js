b90e8f62cf4dda7bea141884530e07f2f2238da4
John Gillan <jgillan@gmail.com>
2018-01-25 22:58:38 +0000

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <nav className="nav container">
+      <a className="nav-link mr-auto active" href="">Skyview</a>
+      <a className="nav-link" href="">About</a>
+      <a className="nav-link" href="">Contact</a>
+    </nav>
+  )
+}
+
+export default Nav;
