6deeb90bd6ccd9b6ef1edade110589aa89a479c9
John Gillan <jgillan@gmail.com>
2018-01-26 21:09:48 +0000

    added sunset

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import Nav from './components/Nav';
+// import Nav from './components/Nav';
 import Results from './components/Results';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
 import './App.css';
@@ -10,38 +10,22 @@ class App extends Component {
     super ()
     this.state = { 
       address: '',
-      weather: '',
-      astronomy: '',
+      weather: {},
+      astronomy: {},
       lat: '',
       lng: '',
-      route: 'display'
+      route: 'display',
+      chartData: {}
     } 
   }
 
-  apiCall = (latLng) => {
-    this.getWeather(latLng);
-    this.getAstro(latLng);
-    this.setState({
-      route: 'display'
-    })
-  }
-  
-  getAstro = (latLng) => {
-    this.setState({
-      lat: latLng.lat,
-      lng: latLng.lng
-    })
-    axios.get(`http://api.wunderground.com/api/547a76a2afd69505/astronomy/q/${this.state.lat},${this.state.lng}.json`)
-      .then((response) => {
-        this.setState({
-          astronomy: response.data
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      })      
-  }
 
+
+
+  // Api calls
+  apiCall = (latLng) => {
+    this.getWeather(latLng)
+  }  
   getWeather = (latLng) => {
     this.setState({
       lat: latLng.lat,
@@ -51,14 +35,27 @@ class App extends Component {
       .then((response) => {
         this.setState({
           weather: response.data.hourly_forecast
-        })
-        console.log(this.state)
+        }, () => this.getAstro())
+      })
+      .catch((error) => {
+        console.log(error);
+      })      
+  }
+  getAstro = () => {
+    axios.get(`http://api.wunderground.com/api/547a76a2afd69505/astronomy/q/${this.state.lat},${this.state.lng}.json`)
+      .then((response) => {
+        this.setState({
+          astronomy: response.data
+        }, () => this.resultlog())
       })
       .catch((error) => {
         console.log(error);
       })      
   }
 
+
+
+  
   // get location and convert to lat lng
   formSubmitHandler = (event) => {
     event.preventDefault()
@@ -75,6 +72,159 @@ class App extends Component {
     })
   }
 
+  resultlog = () => {
+    console.log(this.state)
+    const sunset = (
+      'Sunset - ' + Number(this.state.astronomy.sun_phase.sunset.hour - 12) + ':' + 
+      this.state.astronomy.sun_phase.sunset.minute + ' PM'
+    )
+    const sunrise = (
+      'Sunrise - ' + this.state.astronomy.sun_phase.sunrise.hour + ':' + 
+      this.state.astronomy.sun_phase.sunrise.minute + ' AM'
+    )
+    console.log(sunset, sunrise)
+    const weather = this.state.weather
+    const cloudArray = (
+      weather[0].sky + ',' +
+      weather[1].sky + ',' +
+      weather[2].sky + ',' +
+      weather[3].sky + ',' +
+      weather[4].sky + ',' +
+      weather[5].sky + ',' +
+      weather[6].sky + ',' +
+      weather[7].sky + ',' +
+      weather[8].sky + ',' +
+      weather[9].sky + ',' +
+      weather[10].sky + ',' +
+      weather[11].sky + ',' +
+      weather[12].sky + ',' +
+      weather[13].sky + ',' +
+      weather[14].sky + ',' +
+      weather[15].sky + ',' +
+      weather[16].sky + ',' +
+      weather[17].sky + ',' +
+      weather[18].sky + ',' +
+      weather[19].sky + ',' +
+      weather[20].sky + ',' +
+      weather[21].sky + ',' +
+      weather[22].sky + ',' +
+      weather[23].sky + ',' +
+      weather[24].sky
+    ).split(",")
+    const humidityArray = (
+      weather[0].humidity + ',' +
+      weather[1].humidity + ',' +
+      weather[2].humidity + ',' +
+      weather[3].humidity + ',' +
+      weather[4].humidity + ',' +
+      weather[5].humidity + ',' +
+      weather[6].humidity + ',' +
+      weather[7].humidity + ',' +
+      weather[8].humidity + ',' +
+      weather[9].humidity + ',' +
+      weather[10].humidity + ',' +
+      weather[11].humidity + ',' +
+      weather[12].humidity + ',' +
+      weather[13].humidity + ',' +
+      weather[14].humidity + ',' +
+      weather[15].humidity + ',' +
+      weather[16].humidity + ',' +
+      weather[17].humidity + ',' +
+      weather[18].humidity + ',' +
+      weather[19].humidity + ',' +
+      weather[20].humidity + ',' +
+      weather[21].humidity + ',' +
+      weather[22].humidity + ',' +
+      weather[23].humidity + ',' +
+      weather[24].humidity
+    ).split(",")
+    const windArray = (
+      weather[0].wspd.metric + ',' +
+      weather[1].wspd.metric + ',' +
+      weather[2].wspd.metric + ',' +
+      weather[3].wspd.metric + ',' +
+      weather[4].wspd.metric + ',' +
+      weather[5].wspd.metric + ',' +
+      weather[6].wspd.metric + ',' +
+      weather[7].wspd.metric + ',' +
+      weather[8].wspd.metric + ',' +
+      weather[9].wspd.metric + ',' +
+      weather[10].wspd.metric + ',' +
+      weather[11].wspd.metric + ',' +
+      weather[12].wspd.metric + ',' +
+      weather[13].wspd.metric + ',' +
+      weather[14].wspd.metric + ',' +
+      weather[15].wspd.metric + ',' +
+      weather[16].wspd.metric + ',' +
+      weather[17].wspd.metric + ',' +
+      weather[18].wspd.metric + ',' +
+      weather[19].wspd.metric + ',' +
+      weather[20].wspd.metric + ',' +
+      weather[21].wspd.metric + ',' +
+      weather[22].wspd.metric + ',' +
+      weather[23].wspd.metric + ',' +
+      weather[24].wspd.metric
+    ).split(",")
+    const timeArray = (
+      sunset + ',' +
+      weather[0].FCTTIME.civil + ',' +
+      weather[1].FCTTIME.civil + ',' +
+      weather[2].FCTTIME.civil + ',' +
+      weather[3].FCTTIME.civil + ',' +
+      weather[4].FCTTIME.civil + ',' +
+      weather[5].FCTTIME.civil + ',' +
+      weather[6].FCTTIME.civil + ',' +
+      weather[7].FCTTIME.civil + ',' +
+      weather[8].FCTTIME.civil + ',' +
+      weather[9].FCTTIME.civil + ',' +
+      weather[10].FCTTIME.civil + ',' +
+      weather[11].FCTTIME.civil + ',' +
+      weather[12].FCTTIME.civil + ',' +
+      weather[13].FCTTIME.civil + ',' +
+      weather[14].FCTTIME.civil + ',' +
+      weather[15].FCTTIME.civil + ',' +
+      weather[16].FCTTIME.civil + ',' +
+      weather[17].FCTTIME.civil + ',' +
+      weather[18].FCTTIME.civil + ',' +
+      weather[19].FCTTIME.civil + ',' +
+      weather[20].FCTTIME.civil + ',' +
+      weather[21].FCTTIME.civil + ',' +
+      weather[22].FCTTIME.civil + ',' +
+      weather[23].FCTTIME.civil + ',' +
+      weather[24].FCTTIME.civil + ',' +
+      sunrise
+    ).split(",")
+
+    console.log(timeArray)
+
+
+    this.setState({
+      chartData: {
+        labels: timeArray,
+        datasets:[
+          {
+            pointRadius: 0,
+            label: 'Cloud cover (%)',
+            data: cloudArray,
+            backgroundColor: 'rgba(13, 26, 124, 0.2)'
+          },
+          {
+            pointRadius: 0,
+            label: 'Humidity (%)',
+            data: humidityArray,
+            backgroundColor: 'rgba(238, 255, 0, 0.3)'
+          },
+          {
+            pointRadius: 0,
+            label: 'Wind speed (km/hr)',
+            data: windArray,
+            backgroundColor: 'gray'
+          }
+        ]
+      }
+    })
+  }
+
   render() {
     // places autocomplete
     const inputProps = {
@@ -84,7 +234,7 @@ class App extends Component {
 
     return (
       <div>
-        <Nav />
+        {/* <Nav /> */}
         <div className="container">
           <div className="row">
             <div className="col-md-3">
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,133 +1,40 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Line } from 'react-chartjs-2';
 
-class Chart extends Component {
-  constructor (props) {
-    super (props)
-    this.state = {
-      chartData: {
-        labels: ['16:00','17:00','18:00', '19:00', '20:00', '21:00', '22:00', '23:00','00:00','01:00','02:00','03:00','04:00','05:00','06:00','07:00','08:00'],
-        datasets:[
-          {
-            pointRadius: 0,
-            label: 'Cloud cover',
-            data:[],
-            backgroundColor: 'red'
+const Chart = (props) => {
+  return (
+    <div className="chart">
+      <Line
+        data={props.state.chartData}
+        options={{            
+          legend: {
+            display: true
           },
-          {
-            pointRadius: 0,
-            label: 'Humidity',
-            data:[]
-          },
-          {
-            pointRadius: 0,
-            label: 'Wind speed',
-            data:[]
-          }
-        ]
-      }
-    }
-  }
-
-  resultlog = (props) => {
-    const sunset = (
-      this.props.state.astronomy.sun_phase.sunset.hour + ':' + 
-      this.props.state.astronomy.sun_phase.sunset.minute
-    )
-    const sunrise = (
-      this.props.state.astronomy.sun_phase.sunrise.hour + ':' + 
-      this.props.state.astronomy.sun_phase.sunrise.minute
-    )
-    console.log(sunrise, sunset)
-    const weather = this.props.state.weather
-    const cloudArray = (
-         weather[0].sky + ',' +
-         weather[1].sky + ',' +
-         weather[2].sky + ',' +
-         weather[3].sky + ',' +
-         weather[4].sky + ',' +
-         weather[5].sky + ',' +
-         weather[6].sky + ',' +
-         weather[7].sky + ',' +
-         weather[8].sky + ',' +
-         weather[9].sky
-    ).split(",")
-    const timeArray = (
-      weather[0].FCTTIME.civil + ',' +
-      weather[1].FCTTIME.civil + ',' +
-      weather[2].FCTTIME.civil + ',' +
-      weather[3].FCTTIME.civil + ',' +
-      weather[4].FCTTIME.civil + ',' +
-      weather[5].FCTTIME.civil + ',' +
-      weather[6].FCTTIME.civil + ',' +
-      weather[7].FCTTIME.civil + ',' +
-      weather[8].FCTTIME.civil + ',' +
-      weather[9].FCTTIME.civil
- ).split(",")
-    console.log(timeArray)
-
-
-    this.setState({
-      chartData: {
-        labels: timeArray,
-        datasets:[
-          {
-            pointRadius: 0,
-            label: 'Cloud cover',
-            data: cloudArray,
-            backgroundColor: 'red'
-          },
-          {
-            pointRadius: 0,
-            label: 'Humidity',
-            data:[]
-          },
-          {
-            pointRadius: 0,
-            label: 'Wind speed',
-            data:[]
-          }
-        ]
-      }
-    })
-  }
-
-  render () {
-    return (
-      <div className="chart">
-        <Line
-          data={this.state.chartData}
-          options={{            
-            legend: {
-              display: true
-            },
-            title: {
-              display: true,
-              text: 'Custom Chart Title'
-            },  
-            scales: {              
-              xAxes: [{
-                gridLines: {
-                  display: false
-                }
-              }],
-              yAxes: [{
-                ticks: {
-                  beginatZero: true,
-                  max: 100,
-                  min: 0
-                },
-                gridLines: {
-                  display: false
-                }
-              }]
-            }        
-          }}
-        />
-      <button onClick={this.resultlog}>Go</button>
-      </div>      
-    )
-  }  
+          title: {
+            display: true,
+            text: 'Tonight'
+          },  
+          scales: {              
+            xAxes: [{
+              gridLines: {
+                display: false
+              }
+            }],
+            yAxes: [{
+              ticks: {
+                beginatZero: true,
+                max: 100,
+                min: 0
+              },
+              gridLines: {
+                display: false
+              }
+            }]
+          }        
+        }}
+      />
+    </div>      
+  )  
 }
 
 export default Chart;
